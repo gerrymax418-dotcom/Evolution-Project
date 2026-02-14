f2fadb5509b74494ddafc1174cd6dd20873f6514
gerrymax418-dotcom <gerrymax418@gmail.com>
2026-02-14 11:01:37 +0600

    Make the stuff dissapear upon collision and added escape speed.

--- Assets/Script/Subject.cs
+++ Assets/Script/Subject.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.LowLevelPhysics2D;
 
 public class Subject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+    public float detectionRadius = 5f;
+    public float normalSpeed = 3.5f;
+    public float escapeSpeed = 6f;
+
+    private NavMeshAgent agent;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = normalSpeed;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        DetectEnemies();
+    }
+
+    void DetectEnemies()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, detectionRadius);
+
+        bool enemyNearby = false;
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Enemy"))
+            {
+                enemyNearby = true;
+                break;
+
+            }
+        }
+        if (enemyNearby)
+            agent.speed = escapeSpeed;
+        else
+                agent.speed = normalSpeed;
+        }
         
     }
-}
